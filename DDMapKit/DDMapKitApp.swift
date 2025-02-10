b78f1972e338995363de90fa82271ffda055c408
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-02-10 21:13:44 +0900

    Initial Commit

---
+++ DDMapKit/DDMapKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  DDMapKitApp.swift
+//  DDMapKit
+//
+//  Created by joe on 2/10/25.
+//
+
+import SwiftUI
+
+@main
+struct DDMapKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
